5465289ac324440f9692e5229d30b76dc75775b2
kong_b <kong_b@mti.co.jp>
2014-04-10 15:08:53 +0800

    単体テストベース

---
+++ TenkiDemo.Test/ViewModels/HomeViewModelTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using TenkiDemo.ViewModels;
+
+namespace TenkiDemo.Test
+{
+	[TestFixture ()]
+	public class HomeViewModelTest
+	{
+		private HomeViewModel target;
+
+		[SetUp ()]
+		public void SetUp ()
+		{
+			target = new HomeViewModel ();
+		}
+
+		[TearDown ()]
+		public void TearDown ()
+		{
+		}
+
+		[Test ()]
+		public void GetApiDataTest01 ()
+		{
+			Assert.AreEqual ("Hello World!!", target.GetApiData ());
+		}
+	}
+}
+
