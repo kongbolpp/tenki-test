50ec0808d17a1733e609e77d3b522d75fe3d9daf
sushu <sushu@liandisys.com.cn>
2014-04-14 14:05:37 +0800

    ソースを追加する

--- TenkiDemo/ViewModels/HomeViewModel.cs
+++ TenkiDemo/ViewModels/HomeViewModel.cs
@@ -1,17 +1,46 @@
 ﻿using System;
+using TenkiDemo.Utilities;
+using System.Threading.Tasks;
 
 namespace TenkiDemo.ViewModels
 {
 	public class HomeViewModel : ViewModelBase
 	{
+		readonly IHomeService service;
+		string cityCode;
+
 		public HomeViewModel ()
 		{
-		
+			service = ServiceContainer.Resolve<IHomeService> ();
 		}
 
-		public string GetApiData ()
+		/// <summary>
+		/// cityCode property
+		/// </summary>
+		public string CityCode
 		{
-			return "Hello World!~";
+			get { return cityCode; }
+			set
+			{
+				cityCode = value;
+				Validate ();
+				OnPropertyChanged ("CityCode");
+			}
+		}
+
+		/// <summary>
+		/// Performs an asynchronous login
+		/// </summary>
+		/// <returns></returns>
+		public Task<bool> HomeAsync ()
+		{
+			IsBusy = true;
+			return service
+					.HomeAsync (cityCode)
+					.ContinueOnCurrentThread (t => {
+						IsBusy = false; 
+						return t.Result;
+					});
 		}
 	}
 }
