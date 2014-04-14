360814dada263296304f18abbe9a034493e49fd2
unknown <lusy@ldns-dt-2659.sunjapan.com.cn>
2014-04-14 10:35:06 +0800

    HomeViewModel作成，json.net获取weather信息。

--- TenkiDemo/Data/HomeService.cs
+++ TenkiDemo/Data/HomeService.cs
@@ -19,6 +19,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using TenkiDemo.ViewModels;
+using Newtonsoft.Json;
 
 	namespace TenkiDemo.Data {
 
@@ -29,6 +31,18 @@ using System.Threading.Tasks;
 		public class HomeService : IHomeService {
 
         private const int Sleep = 1000;
+		public HomeViewModel GetWeather (string strCityName){
+
+			var webClient = new System.Net.WebClient();
+			// 101010100 
+			var result = webClient.DownloadString("http://www.weather.com.cn/data/sk/"+strCityName+".html");
+			//透过JSON.net 反序列化为HomeViewModel对象
+			var weather = JsonConvert.DeserializeObject<HomeViewModel>(result);
+			//印出 id and name
+
+			Console.Write ("weather.city={0},weather.cityid={1}",weather.city,weather.cityid ); 
+			return weather;
+		}
 
 			public Task<bool> HomeAsync (string username, string password, CancellationToken cancellationToken = default(CancellationToken))
         {
