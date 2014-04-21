4f09f6cc29c3fd4936f4b0031757b5d291958efe
unknown <lusy@ldns-dt-2659.sunjapan.com.cn>
2014-04-21 09:33:49 +0800

    データの取得するUrl改修する。

--- TenkiDemo/Data/IHomeService.cs
+++ TenkiDemo/Data/IHomeService.cs
@@ -1,19 +1,4 @@
-﻿//
-//  Copyright 2012  Xamarin Inc.
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
-//        http://www.apache.org/licenses/LICENSE-2.0
-//
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,14 +9,17 @@ using System.Collections;
 
 namespace TenkiDemo {
 
-    /// <summary>
-    /// Interface providing backend data to ViewModels
-    /// </summary>
+	/// <summary>
+	/// I home service
+	/// </summary>
 	public interface IHomeService {
 
-        /// <summary>
-		/// Asynchronous home
-        /// </summary>
+		/// <summary>
+		/// ホームデータ取得
+		/// </summary>
+		/// <returns>取得したデータ</returns>
+		/// <param name="cityCode">地域コード</param>
+		/// <param name="cancellationToken">キャンセルトークン</param>
 		Task<Hashtable> HomeAsync (string cityCode, CancellationToken cancellationToken = default(CancellationToken));
 
 		void GetWeather (string strCityName);
